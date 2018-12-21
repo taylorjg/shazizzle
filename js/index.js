0e42901c7579d40ef155f4e61ed7ce22d2575ee9
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-12-21 00:00:29 +0000

    Add chart showing FFT

--- js/index.js
+++ js/index.js
@@ -1,43 +1,48 @@
-const SAMPLE_RATE = 44100
+const SAMPLE_RATE = 4096
 const SECONDS = 1
 
-// const response = await axios.get('440Hz_44100Hz_16bit_05sec.mp3')
+const findBound = (xs, f) => xs.reduce((acc, x) => f(x, acc) ? x : acc)
+const upperBound = xs => Math.ceil(findBound(xs, R.gt))
+const lowerBound = xs => Math.floor(findBound(xs, R.lt))
 
-const findBound = (xs, f) => xs.reduce((acc, x) => f(x, acc) ? x : acc, 0)
+const makeOscillator = (context, hz) => {
+  const oscillator = new OscillatorNode(context, { frequency: hz })
+  oscillator.connect(context.destination)
+  return oscillator
+}
+
+const startOscillator = when => oscillator => oscillator.start(when)
+const stopOscillator = when => oscillator => oscillator.stop(when)
 
 const main = async () => {
+
   const audioContext = new OfflineAudioContext(1, SAMPLE_RATE * SECONDS, SAMPLE_RATE)
 
-  const makeOscillator = (ctx, hz) => {
-    const oscillator = new OscillatorNode(ctx, { frequency: hz })
-    oscillator.connect(ctx.destination)
-    return oscillator
-  }
-
   const oscillators = [
-    makeOscillator(audioContext, 2),
-    makeOscillator(audioContext, 4),
-    makeOscillator(audioContext, 6),
-    makeOscillator(audioContext, 8)
+    makeOscillator(audioContext, 20),
+    makeOscillator(audioContext, 200),
+    makeOscillator(audioContext, 440),
+    makeOscillator(audioContext, 1000)
   ]
 
-  const startOscillator = when => oscillator => oscillator.start(when)
-  const stopOscillator = when => oscillator => oscillator.stop(when)
+  const analyser = new AnalyserNode(audioContext, { fftSize: SAMPLE_RATE })
+  oscillators.forEach(oscillator => oscillator.connect(analyser))
 
-  oscillators.forEach(startOscillator(0.25))
-  oscillators.forEach(stopOscillator(0.75))
+  oscillators.forEach(startOscillator(0))
+  oscillators.forEach(stopOscillator(1))
 
   const audioBuffer = await audioContext.startRendering()
   const data = audioBuffer.getChannelData(0)
-  console.dir(data)
 
-  const config = {
+  const frequencyData = new Uint8Array(analyser.frequencyBinCount)
+  analyser.getByteFrequencyData(frequencyData)
+
+  const config1 = {
     type: 'line',
     data: {
       labels: data.map((_, index) => index),
       datasets: [{
         borderColor: 'orange',
-        bordeWidth: 0.25,
         data,
         fill: false,
       }]
@@ -52,16 +57,47 @@ const main = async () => {
       scales: {
         yAxes: [{
           ticks: {
-            min: findBound(data, R.lt),
-            max: findBound(data, R.gt)
+            min: lowerBound(data),
+            max: upperBound(data)
           }
         }]
       }
     }
   }
 
-  const chartContext = document.getElementById('canvas').getContext('2d')
-  new Chart(chartContext, config)
+  const chartContext1 = document.getElementById('canvas1').getContext('2d')
+  new Chart(chartContext1, config1)
+
+  const config2 = {
+    type: 'line',
+    data: {
+      labels: frequencyData.map((_, index) => index),
+      datasets: [{
+        borderColor: 'orange',
+        data: frequencyData,
+        fill: false,
+      }]
+    },
+    options: {
+      animation: {
+        duration: 0
+      },
+      legend: {
+        display: false
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            min: lowerBound(frequencyData),
+            max: upperBound(frequencyData)
+          }
+        }]
+      }
+    }
+  }
+
+  const chartContext2 = document.getElementById('canvas2').getContext('2d')
+  new Chart(chartContext2, config2)
 }
 
 main()
