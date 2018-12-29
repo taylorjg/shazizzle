2ce43afa1b96322cd8bd8226414c2e39a818ac40
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-12-29 12:00:10 +0000

    Create utils module

--- js/index.js
+++ js/index.js
@@ -1,126 +1,64 @@
-const SECONDS = 2
+const SECONDS = 1
 
-const createCheckboxes = (parentId, name, values) => {
-  const parent = document.getElementById(parentId)
-  return values.map(value => {
-    const label = document.createElement('label')
-    label.setAttribute('class', 'checkbox-inline')
-    const input = document.createElement('input')
-    input.setAttribute('type', 'checkbox')
-    input.setAttribute('id', `${name}${value}`)
-    input.setAttribute('value', value)
-    const text = document.createTextNode(value)
-    label.appendChild(input)
-    label.appendChild(text)
-    parent.appendChild(label)
-    return input
-  })
-}
-
-const createRadioButtons = (parentId, name, values) => {
-  const parent = document.getElementById(parentId)
-  return values.map(value => {
-    const label = document.createElement('label')
-    label.setAttribute('class', 'radio-inline')
-    const input = document.createElement('input')
-    input.setAttribute('type', 'radio')
-    input.setAttribute('name', name)
-    input.setAttribute('id', `${name}${value}`)
-    input.setAttribute('value', value)
-    const text = document.createTextNode(value)
-    label.appendChild(input)
-    label.appendChild(text)
-    parent.appendChild(label)
-    return input
-  })
-}
-
-const setCheckedRadioButton = (buttons, value) =>
-  buttons.forEach(button =>
-    button.checked = value === Number(button.value))
-
-const getCheckedRadioButton = buttons =>
-  buttons
-    .map(button => button.checked ? Number(button.value) : undefined)
-    .filter(R.identity)[0]
-
-const setCheckedCheckboxes = (buttons, values) =>
-  buttons.forEach(button =>
-    button.checked = values.includes(Number(button.value)))
-
-const getCheckedCheckboxes = buttons =>
-  buttons
-    .map(button => button.checked ? Number(button.value) : undefined)
-    .filter(R.identity)
-
-const buttonsOnChange = (buttons, fn) =>
-  buttons.forEach(button => button.addEventListener('change', fn))
-
-const sampleRateValues = [4096, 8192, 16384, 32768]
+const sampleRateValues = [4096, 8192, 16384, 32768, 44100, 1024 * 44]
 const fftSizeValues = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768]
 const gainValues = [0.125, 0.25, 0.5, 0.75, 1.0]
 const frequencyValues = [1, 2, 4, 6, 8, 440, 1000, 2000]
 
-let currentSampleRate = 4096
+let currentSampleRate = 1024 * 44
 let currentFftSize = 1024
-let currentGain = 0.5
+let currentGain = 0.25
 let currentFrequencies = [440, 1000, 2000]
 
-const sampleRateRadioButtons = createRadioButtons(
+const sampleRateRadioButtons = U.createRadioButtons(
   'sampleRates',
   'sampleRate',
   sampleRateValues)
 
-const fftSizeRadioButtons = createRadioButtons(
+const fftSizeRadioButtons = U.createRadioButtons(
   'fftSizes',
   'fftSize',
   fftSizeValues)
 
-const gainRadioButtons = createRadioButtons(
+const gainRadioButtons = U.createRadioButtons(
   'gains',
   'gain',
   gainValues)
 
-const frequencyCheckboxes = createCheckboxes(
+const frequencyCheckboxes = U.createCheckboxes(
   'frequencies',
   'frequency',
   frequencyValues)
 
 const onSampleRateChange = () => {
-  currentSampleRate = getCheckedRadioButton(sampleRateRadioButtons)
+  currentSampleRate = U.getCheckedRadioButton(sampleRateRadioButtons)
   drawCharts(currentSampleRate, currentFftSize, currentGain, currentFrequencies)
 }
 
 const onFFtSizeChange = () => {
-  currentFftSize = getCheckedRadioButton(fftSizeRadioButtons)
+  currentFftSize = U.getCheckedRadioButton(fftSizeRadioButtons)
   drawCharts(currentSampleRate, currentFftSize, currentGain, currentFrequencies)
 }
 
 const onGainChange = () => {
-  currentGain = getCheckedRadioButton(gainRadioButtons)
+  currentGain = U.getCheckedRadioButton(gainRadioButtons)
   drawCharts(currentSampleRate, currentFftSize, currentGain, currentFrequencies)
 }
 
 const onFrequencyChange = () => {
-  currentFrequencies = getCheckedCheckboxes(frequencyCheckboxes)
+  currentFrequencies = U.getCheckedCheckboxes(frequencyCheckboxes)
   drawCharts(currentSampleRate, currentFftSize, currentGain, currentFrequencies)
 }
 
-setCheckedRadioButton(sampleRateRadioButtons, currentSampleRate)
-setCheckedRadioButton(fftSizeRadioButtons, currentFftSize)
-setCheckedRadioButton(gainRadioButtons, currentGain)
-setCheckedCheckboxes(frequencyCheckboxes, currentFrequencies)
+U.setCheckedRadioButton(sampleRateRadioButtons, currentSampleRate)
+U.setCheckedRadioButton(fftSizeRadioButtons, currentFftSize)
+U.setCheckedRadioButton(gainRadioButtons, currentGain)
+U.setCheckedCheckboxes(frequencyCheckboxes, currentFrequencies)
 
-buttonsOnChange(sampleRateRadioButtons, onSampleRateChange)
-buttonsOnChange(fftSizeRadioButtons, onFFtSizeChange)
-buttonsOnChange(gainRadioButtons, onGainChange)
-buttonsOnChange(frequencyCheckboxes, onFrequencyChange)
-
-const findBound = (xs, f) => xs.reduce((acc, x) => f(x, acc) ? x : acc)
-const upperBound = xs => Math.ceil(findBound(xs, R.gt))
-const lowerBound = xs => Math.floor(findBound(xs, R.lt))
-const getIndices = xs => xs.map((_, index) => index)
-const range = n => Array.from(Array(n).keys())
+U.buttonsOnChange(sampleRateRadioButtons, onSampleRateChange)
+U.buttonsOnChange(fftSizeRadioButtons, onFFtSizeChange)
+U.buttonsOnChange(gainRadioButtons, onGainChange)
+U.buttonsOnChange(frequencyCheckboxes, onFrequencyChange)
 
 const makeOscillatorNode = (audioContext, gainNode) => frequency => {
   const oscillatorNode = new OscillatorNode(audioContext, { frequency })
@@ -138,86 +76,17 @@ const drawCharts = async (sampleRate, fftSize, gain, frequencies) => {
   gainNode.connect(audioContext.destination)
   const analyserNode = new AnalyserNode(audioContext, { fftSize })
   gainNode.connect(analyserNode)
-  oscillators.forEach(startOscillator(audioContext.currentTime + 1))
-  oscillators.forEach(stopOscillator(audioContext.currentTime + 2))
+  oscillators.forEach(startOscillator(audioContext.currentTime + SECONDS * 43 / 44))
+  oscillators.forEach(stopOscillator(audioContext.currentTime + SECONDS))
   const audioBuffer = await audioContext.startRendering()
   const channelData = audioBuffer.getChannelData(0)
   const timeDomainData = new Uint8Array(analyserNode.frequencyBinCount)
   const frequencyData = new Uint8Array(analyserNode.frequencyBinCount)
   analyserNode.getByteTimeDomainData(timeDomainData)
   analyserNode.getByteFrequencyData(frequencyData)
-  drawChart('chart1', channelData)
-  drawChart('chart2', timeDomainData)
-  drawChart('chart3', frequencyData)
-}
-
-const drawChart = (elementId, data) => {
-  // const indices = getIndices(data)
-  const indices = range(data.length)
-  // console.log(`data: ${JSON.stringify(data, null, 2)}`)
-  // console.log(`[drawChart] elementId: ${elementId}; indices: ${R.head(indices)} - ${R.last(indices)}; data.length: ${data.length}`)
-  const config = {
-    type: 'line',
-    data: {
-      labels: indices,
-      datasets: [{
-        borderColor: 'orange',
-        borderWidth: 0.5,
-        pointStyle: 'line',
-        radius: 1,
-        data,
-        fill: false,
-      }]
-    },
-    options: {
-      events: [],
-      animation: {
-        duration: 0
-      },
-      legend: {
-        display: false
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            min: lowerBound(data),
-            max: upperBound(data)
-          }
-        }]
-      }
-    }
-  }
-
-  const chart = document.getElementById(elementId)
-  new Chart(chart, config)
+  U.drawChart('chart1', channelData)
+  U.drawChart('chart2', timeDomainData)
+  U.drawChart('chart3', frequencyData)
 }
 
 drawCharts(currentSampleRate, currentFftSize, currentGain, currentFrequencies)
-
-// document.getElementById('go').addEventListener('click', () => {
-//   drawCharts(currentSampleRate, currentFftSize, currentFrequencies)
-// })
-
-const recordSample = async duration => {
-  const SAMPLE_RATE = 4096
-  const constraints = {
-    audio: {
-      sampleRate: {
-        exact: SAMPLE_RATE
-      }
-    }
-  }
-  const mediaStream = await navigator.mediaDevices.getUserMedia(constraints)
-  const mediaRecorder = new MediaRecorder(mediaStream, { audioBitsPerSecond: SAMPLE_RATE })
-  const chunks = []
-  mediaRecorder.ondataavailable = e => {
-    console.error(`[mediaRecorder.ondataavailable] e.data.length: ${e.data.length}`)
-    chunks.push(e.data)
-  }
-  mediaRecorder.onerror = e => {
-    console.error(`[mediaRecorder.onerror] ${e}`)
-  }
-  mediaRecorder.start()
-  await new Promise(resolve => setTimeout(resolve, duration))
-  mediaRecorder.stop()
-}
---
+++ js/utils.js
@@ -0,0 +1,110 @@
+const U = {};
+(function (exports) {
+
+  const createCheckboxes = (parentId, name, values) => {
+    const parent = document.getElementById(parentId)
+    return values.map(value => {
+      const label = document.createElement('label')
+      label.setAttribute('class', 'checkbox-inline')
+      const input = document.createElement('input')
+      input.setAttribute('type', 'checkbox')
+      input.setAttribute('id', `${name}${value}`)
+      input.setAttribute('value', value)
+      const text = document.createTextNode(value)
+      label.appendChild(input)
+      label.appendChild(text)
+      parent.appendChild(label)
+      return input
+    })
+  }
+
+  const createRadioButtons = (parentId, name, values) => {
+    const parent = document.getElementById(parentId)
+    return values.map(value => {
+      const label = document.createElement('label')
+      label.setAttribute('class', 'radio-inline')
+      const input = document.createElement('input')
+      input.setAttribute('type', 'radio')
+      input.setAttribute('name', name)
+      input.setAttribute('id', `${name}${value}`)
+      input.setAttribute('value', value)
+      const text = document.createTextNode(value)
+      label.appendChild(input)
+      label.appendChild(text)
+      parent.appendChild(label)
+      return input
+    })
+  }
+
+  const setCheckedRadioButton = (buttons, value) =>
+    buttons.forEach(button =>
+      button.checked = value === Number(button.value))
+
+  const getCheckedRadioButton = buttons =>
+    buttons
+      .map(button => button.checked ? Number(button.value) : undefined)
+      .filter(R.identity)[0]
+
+  const setCheckedCheckboxes = (buttons, values) =>
+    buttons.forEach(button =>
+      button.checked = values.includes(Number(button.value)))
+
+  const getCheckedCheckboxes = buttons =>
+    buttons
+      .map(button => button.checked ? Number(button.value) : undefined)
+      .filter(R.identity)
+
+  const buttonsOnChange = (buttons, fn) =>
+    buttons.forEach(button => button.addEventListener('change', fn))
+
+  const findBound = (xs, f) => xs.reduce((acc, x) => f(x, acc) ? x : acc)
+  const upperBound = xs => Math.ceil(findBound(xs, R.gt))
+  const lowerBound = xs => Math.floor(findBound(xs, R.lt))
+
+  const drawChart = (elementId, data) => {
+    const indices = R.range(0, data.length)
+    const config = {
+      type: 'line',
+      data: {
+        labels: indices,
+        datasets: [{
+          borderColor: 'orange',
+          borderWidth: 0.5,
+          pointStyle: 'line',
+          radius: 1,
+          data,
+          fill: false,
+        }]
+      },
+      options: {
+        events: [],
+        animation: {
+          duration: 0
+        },
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              min: lowerBound(data),
+              max: upperBound(data)
+            }
+          }]
+        }
+      }
+    }
+
+    const chart = document.getElementById(elementId)
+    new Chart(chart, config)
+  }
+
+  exports.createCheckboxes = createCheckboxes
+  exports.createRadioButtons = createRadioButtons
+  exports.getCheckedCheckboxes = getCheckedCheckboxes
+  exports.setCheckedCheckboxes = setCheckedCheckboxes
+  exports.getCheckedRadioButton = getCheckedRadioButton
+  exports.setCheckedRadioButton = setCheckedRadioButton
+  exports.buttonsOnChange = buttonsOnChange
+  exports.drawChart = drawChart
+})(U)
