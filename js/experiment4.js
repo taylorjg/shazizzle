43dce786257c1dae03991fcdd8ec5764d6ef4814
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-01-01 10:31:17 +0000

    Add stepping controls to experiment 4

--- js/experiment4.js
+++ js/experiment4.js
@@ -1,5 +1,21 @@
-const onRecord = async e => {
-  e.target.disabled = true
+const recordButton = document.getElementById('record')
+const controlPanel = document.getElementById('controlPanel')
+const fastBackward44 = document.getElementById('fastBackward44')
+const fastBackward10 = document.getElementById('fastBackward10')
+const stepBackward = document.getElementById('stepBackward')
+const stepForward = document.getElementById('stepForward')
+const fastForward10 = document.getElementById('fastForward10')
+const fastForward44 = document.getElementById('fastForward44')
+const currentSliverLabel = document.getElementById('currentSliverLabel')
+const maxSliverLabel = document.getElementById('maxSliverLabel')
+
+let currentSliver = 0
+let maxSliver = 0
+let audioBuffer = null
+
+const onRecord = async () => {
+  recordButton.disabled = true
+  controlPanel.style.display = 'none'
   const mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true })
   const mediaRecorder = new MediaRecorder(mediaStream)
   const chunks = []
@@ -11,14 +27,41 @@ const onRecord = async e => {
     const response = await axios.get(url, config)
     const data = response.data
     const audioContext = new OfflineAudioContext({ length: 44100, sampleRate: 44100 })
-    const audioBuffer = await audioContext.decodeAudioData(data)
-    U.visualiseSliver(audioBuffer, 10, 'chart1', 'chart2')
-    e.target.disabled = false
+    audioBuffer = await audioContext.decodeAudioData(data)
+    currentSliver = 0
+    maxSliver = Math.ceil(audioBuffer.duration / U.SLIVER_SIZE)
+    changeSliver(0)()
+    recordButton.disabled = false
     mediaStream.getTracks().forEach(track => track.stop())
+    controlPanel.style.display = 'block'
   }
   mediaRecorder.start()
-  await U.delay(1000)
+  await U.delay(2000)
   mediaRecorder.stop()
 }
 
-document.getElementById('record').addEventListener('click', onRecord)
+const updateControls = () => {
+  fastBackward44.disabled = currentSliver < 44
+  fastBackward10.disabled = currentSliver < 10
+  stepBackward.disabled = currentSliver < 1
+  stepForward.disabled = currentSliver >= (maxSliver - 1)
+  fastForward10.disabled = currentSliver >= (maxSliver - 10)
+  fastForward44.disabled = currentSliver >= (maxSliver - 44)
+}
+
+const changeSliver = adjustment => () => {
+  currentSliver += adjustment
+  updateControls()
+  currentSliverLabel.innerText = `${currentSliver + 1}`
+  maxSliverLabel.innerText = `${maxSliver}`
+  U.visualiseSliver(audioBuffer, currentSliver, 'chart1', 'chart2')
+}
+
+fastBackward44.addEventListener('click', changeSliver(-44))
+fastBackward10.addEventListener('click', changeSliver(-10))
+stepBackward.addEventListener('click', changeSliver(-1))
+stepForward.addEventListener('click', changeSliver(+1))
+fastForward10.addEventListener('click', changeSliver(+10))
+fastForward44.addEventListener('click', changeSliver(+44))
+
+recordButton.addEventListener('click', onRecord)
