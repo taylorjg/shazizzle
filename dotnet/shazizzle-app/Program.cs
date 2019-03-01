b0570bd1f7c9f833df896fdd39443f17aa60ee79
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-03-01 04:01:35 +0000

    Make a start on C# processing of MP3 files

---
+++ dotnet/shazizzle-app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ShazizzleLib;
+
+namespace ShazizzleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Analyzer.analyze(440, "../js/signals/440.pcm");
+            Analyzer.analyze(1000, "../js/signals/1000.pcm");
+            Analyzer.analyze(10000, "../js/signals/10000.pcm");
+        }
+    }
+}
---
+++ dotnet/shazizzle-lib/Analyzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+
+namespace ShazizzleLib
+{
+    public static class Analyzer
+    {
+        public static void analyze(int frequency, string path)
+        {
+            var bytes = File.ReadAllBytes(path);
+            var samples = bytes
+                .Skip(1024 * 40)
+                .Take(1024)
+                .Select(by => new Complex32(by, 0))
+                .ToArray();
+            Fourier.Forward(samples);
+            Console.WriteLine($"After FFT: {FormatSamples(samples)}");
+            Console.WriteLine(new String('-', 80));
+            SaveToCsvFile($"../js/signals/{frequency}.csv", samples);
+        }
+
+        private static void SaveToCsvFile(string path, Complex32[] samples)
+        {
+            var headings = new[] { "x,y" };
+            var rows = samples.Skip(1).Take(512).Select((c, i) => $"{i},{Complex32.Abs(c)}");
+            var contents = string.Join(Environment.NewLine, headings.Concat(rows));
+            File.WriteAllText(path, contents);
+        }
+
+        private static string FormatSamples(Complex32[] samples)
+        {
+            var strings = samples.Select((c, i) => $"[{i}]: {c}");
+            return string.Join(Environment.NewLine, strings);
+        }
+    }
+}
