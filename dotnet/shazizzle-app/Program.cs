0aa4c273647184eabeeb1789566e388e35a7935b
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-03-02 13:18:11 +0000

    C# FFT of generated 440 Hz signal

--- dotnet/shazizzle-app/Program.cs
+++ dotnet/shazizzle-app/Program.cs
@@ -7,9 +7,8 @@ namespace ShazizzleApp
     {
         static void Main(string[] args)
         {
-            Analyzer.analyze(440, "../js/signals/440.pcm");
-            Analyzer.analyze(1000, "../js/signals/1000.pcm");
-            Analyzer.analyze(10000, "../js/signals/10000.pcm");
+            Analyser.analysePcmFile(440, "../js/signals/440.pcm");
+            Analyser.analyseGenerated(440);
         }
     }
 }
--- dotnet/shazizzle-lib/Analyzer.cs
+++ dotnet/shazizzle-lib/Analyzer.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 using System.IO;
 using MathNet.Numerics;
 using MathNet.Numerics.IntegralTransforms;
 
 namespace ShazizzleLib
 {
-    public static class Analyzer
+    public static class Analyser
     {
-        public static void analyze(int frequency, string path)
+        public static void analyseGenerated(int frequency)
+        {
+            var samples = Generate.Sinusoidal(1024, 44100, frequency, 1.0)
+                .Select(sample => new Complex32((float)sample, 0))
+                .ToArray();
+            Fourier.Forward(samples);
+            var results = samples.Take(256).Select(result => result.Magnitude);
+            Console.WriteLine($"FFT results: {FormatResults(results)}");
+            Console.WriteLine(new String('-', 80));
+            SaveResultsToCsvFile($"../js/signals/{frequency}_from_generated_data.csv", results);
+        }
+
+        public static void analysePcmFile(int frequency, string path)
         {
             var bytes = File.ReadAllBytes(path);
             var samples = bytes
@@ -17,22 +30,23 @@ namespace ShazizzleLib
                 .Select(by => new Complex32(by, 0))
                 .ToArray();
             Fourier.Forward(samples);
-            Console.WriteLine($"After FFT: {FormatSamples(samples)}");
+            var results = samples.Take(256).Select(result => result.Magnitude);
+            Console.WriteLine($"FFT results: {FormatResults(results)}");
             Console.WriteLine(new String('-', 80));
-            SaveToCsvFile($"../js/signals/{frequency}.csv", samples);
+            SaveResultsToCsvFile($"../js/signals/{frequency}_from_pcm_file.csv", results);
         }
 
-        private static void SaveToCsvFile(string path, Complex32[] samples)
+        private static void SaveResultsToCsvFile(string path, IEnumerable<float> results)
         {
             var headings = new[] { "x,y" };
-            var rows = samples.Skip(1).Take(512).Select((c, i) => $"{i},{Complex32.Abs(c)}");
+            var rows = results.Select((result, index) => $"{index},{result}");
             var contents = string.Join(Environment.NewLine, headings.Concat(rows));
             File.WriteAllText(path, contents);
         }
 
-        private static string FormatSamples(Complex32[] samples)
+        private static string FormatResults(IEnumerable<float> results)
         {
-            var strings = samples.Select((c, i) => $"[{i}]: {c}");
+            var strings = results.Select((result, index) => $"[{index}]: {result}");
             return string.Join(Environment.NewLine, strings);
         }
     }
