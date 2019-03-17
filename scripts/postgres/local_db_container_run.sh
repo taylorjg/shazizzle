266012a0009248054ea36c03f2624ccbf1014c7f
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-03-17 23:04:40 +0000

    Add remaining PostgreSQL scripts

--- scripts/postgres/local_db_container_run.sh
+++ scripts/postgres/local_db_container_run.sh
@@ -4,9 +4,12 @@ set -euo pipefail
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+export $(cat "$DIR"/.env | xargs)
+
 docker run \
     --name postgres-shazizzle-prep \
     --publish 5432:5432 \
     --volume "$DIR":/db_scripts \
+    --env PGPASSWORD=$HEROKU_DB_PASSWORD \
     --detach \
     postgres:11.2
