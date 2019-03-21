0432f603cb549e9165b1530cdc6cf05bbce4f90a
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-03-21 19:51:50 +0000

    Change show query again - show track title instead of album title

--- scripts/postgres/db_show.sql
+++ scripts/postgres/db_show.sql
@@ -4,9 +4,9 @@
 -- SELECT COUNT(*) FROM track_metadata;
 -- SELECT COUNT(*) FROM track_hashes;
 
-SELECT m.album_title, COUNT(h.id)
+SELECT m.track_title, COUNT(h.id)
 FROM track_hashes AS h
 INNER JOIN track_metadata AS m
 ON h.track_metadata_id = m.id
-GROUP BY m.album_title
+GROUP BY m.id, m.track_title
 ORDER BY count(h.id) DESC;
