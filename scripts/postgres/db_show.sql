811b10b5b119eec1e264a5914e1b976896961628
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-03-21 19:47:53 +0000

    Change PostreSQL show query

--- scripts/postgres/db_show.sql
+++ scripts/postgres/db_show.sql
@@ -1,4 +1,12 @@
-\d track_metadata;
-\d track_hashes;
-SELECT COUNT(*) FROM track_metadata;
-SELECT COUNT(*) FROM track_hashes;
+-- \d track_metadata;
+-- \d track_hashes;
+
+-- SELECT COUNT(*) FROM track_metadata;
+-- SELECT COUNT(*) FROM track_hashes;
+
+SELECT m.album_title, COUNT(h.id)
+FROM track_hashes AS h
+INNER JOIN track_metadata AS m
+ON h.track_metadata_id = m.id
+GROUP BY m.album_title
+ORDER BY count(h.id) DESC;
