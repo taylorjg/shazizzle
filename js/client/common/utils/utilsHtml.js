34c0885cf7aecfe34e42981531c7970872ffa3e6
Administrator <jonathan.taylor.programmer@gmail.com>
2020-02-01 20:29:25 +0000

    Add AudioContext monkey patch to oscillator experiment

--- js/client/common/utils/utilsHtml.js
+++ js/client/common/utils/utilsHtml.js
@@ -1,7 +1,7 @@
 export const createCheckboxes = (parentId, name, values) => {
   const parent = document.getElementById(parentId)
   return values.map(value => {
-    const valueIsObject = !!value.label && Number.isInteger(value.value)
+    const valueIsObject = value.label !== undefined && value.value !== undefined
     const label = document.createElement('label')
     label.setAttribute('class', 'checkbox-inline')
     const input = document.createElement('input')
@@ -19,7 +19,7 @@ export const createCheckboxes = (parentId, name, values) => {
 export const createRadioButtons = (parentId, name, values) => {
   const parent = document.getElementById(parentId)
   return values.map(value => {
-    const valueIsObject = !!value.label && Number.isInteger(value.value)
+    const valueIsObject = value.label !== undefined && value.value !== undefined
     const label = document.createElement('label')
     label.setAttribute('class', 'radio-inline')
     const input = document.createElement('input')
@@ -35,23 +35,68 @@ export const createRadioButtons = (parentId, name, values) => {
   })
 }
 
-export const setCheckedRadioButton = (buttons, value) =>
+export const setCheckedRadioButton = (buttons, value) => {
+  const valueAsString = String(value)
   buttons.forEach(button =>
-    button.checked = value === Number(button.value))
+    button.checked = button.value === valueAsString)
+}
 
 export const getCheckedRadioButton = buttons =>
   buttons
-    .map(button => button.checked ? Number(button.value) : undefined)
-    .filter(R.identity)[0]
+    .map(button => button.checked ? button.value : undefined)
+    .filter(value => value !== undefined)[0]
 
-export const setCheckedCheckboxes = (buttons, values) =>
+export const setCheckedCheckboxes = (buttons, values) => {
+  const valuesAsStrings = values.map(String)
   buttons.forEach(button =>
-    button.checked = values.includes(Number(button.value)))
+    button.checked = valuesAsStrings.includes(button.value))
+}
 
 export const getCheckedCheckboxes = buttons =>
   buttons
-    .map(button => button.checked ? Number(button.value) : undefined)
-    .filter(R.identity)
+    .map(button => button.checked ? button.value : undefined)
+    .filter(value => value !== undefined)
 
-export const buttonsOnChange = (buttons, fn) =>
-  buttons.forEach(button => button.addEventListener('change', fn))
+export const buttonsOnChange = (buttons, handler) =>
+  buttons.forEach(button => button.addEventListener('change', handler))
+
+export const checkboxesOnChange = buttonsOnChange
+export const radioButtonsOnChange = buttonsOnChange
+
+export const createSelect = (parentId, items) => {
+  const parent = document.getElementById(parentId)
+  const select = document.createElement('select')
+  items.forEach(item => {
+    const option = document.createElement('option')
+    option.setAttribute('value', item.value)
+    option.innerText = item.label || item.value
+    select.appendChild(option)
+  })
+  parent.appendChild(select)
+  return select
+}
+
+export const getSelectedValue = select => select.value
+export const setSelectedValue = (select, value) => select.value = value
+
+export const selectOnChange = (select, handler) =>
+  select.addEventListener('change', handler)
+
+export const showErrorPanel = errorMessage => {
+  hideErrorPanel()
+  const parentElement = document.querySelector('body')
+  const template = document.getElementById('error-panel-template')
+  const clone = document.importNode(template.content, true)
+  const errorPanelText = clone.querySelector('.error-panel-text')
+  const closeBtn = clone.querySelector('.close')
+  errorPanelText.textContent = errorMessage
+  closeBtn.addEventListener('click', hideErrorPanel)
+  parentElement.appendChild(clone)
+}
+
+export const hideErrorPanel = () => {
+  const errorPanel = document.getElementById('error-panel')
+  if (errorPanel) {
+    errorPanel.parentNode.removeChild(errorPanel)
+  }
+}
--- js/client/experiments/oscillatorNodes.js
+++ js/client/experiments/oscillatorNodes.js
@@ -1,50 +1,87 @@
+import '../AudioContextMonkeyPatch.js'
 import * as UH from '../common/utils/utilsHtml.js'
 import * as UC from '../common/utils/utilsChart.js'
 
-const frequencyValues = [10, 440, 1000, 2000]
-const gainValues = [0.125, 0.25, 0.5, 0.75, 1.0]
+const SAMPLE_RATES = [44100, 8000]
+const FFT_SIZES = [256, 512, 1024, 2048, 4096, 8192]
+const FREQUENCY_VALUES = [10, 440, 1000, 2000]
+const GAIN_VALUES = [0.125, 0.25, 0.5, 0.75, 1.0]
 
-let currentFrequencies = []
-let currentGain = 1
+const DEFAULT_SAMPLE_RATE = SAMPLE_RATES[0]
+const DEFAULT_FFT_SIZE = FFT_SIZES[2]
+const DEFAULT_FREQUENCIES = [FREQUENCY_VALUES[0], FREQUENCY_VALUES[1], FREQUENCY_VALUES[2]]
+const DEFAULT_GAIN = 0.25
+
+let currentSampleRate = DEFAULT_SAMPLE_RATE
+let currentFftSize = DEFAULT_FFT_SIZE
+let currentFrequencies = DEFAULT_FREQUENCIES
+let currentGain = DEFAULT_GAIN
 
 const onReset = () => {
-  currentFrequencies = []
-  currentGain = 1
+  currentSampleRate = DEFAULT_SAMPLE_RATE
+  currentFftSize = DEFAULT_FFT_SIZE
+  currentFrequencies = DEFAULT_FREQUENCIES
+  currentGain = DEFAULT_GAIN
+  UH.setSelectedValue(sampleRateSelect, currentSampleRate)
+  UH.setSelectedValue(fftSizesSelect, currentFftSize)
   UH.setCheckedCheckboxes(frequencyCheckboxes, currentFrequencies)
   UH.setCheckedRadioButton(gainRadioButtons, currentGain)
-  drawCharts(currentFrequencies, currentGain)
+  updateCharts()
 }
 
 document.getElementById('resetButton').addEventListener('click', onReset)
 
+const sampleRateSelect = UH.createSelect(
+  'sampleRates',
+  SAMPLE_RATES.map(value => ({ value })))
+
+const fftSizesSelect = UH.createSelect(
+  'fftSizes',
+  FFT_SIZES.map(value => ({ value })))
+
 const frequencyCheckboxes = UH.createCheckboxes(
   'frequencies',
   'frequency',
-  frequencyValues)
+  FREQUENCY_VALUES)
 
 const gainRadioButtons = UH.createRadioButtons(
   'gain',
   'gain',
-  gainValues)
+  GAIN_VALUES)
+
+const onSampleRateChange = () => {
+  currentSampleRate = Number(UH.getSelectedValue(sampleRateSelect))
+  updateCharts()
+}
+
+const onFftSizeChange = () => {
+  currentFftSize = Number(UH.getSelectedValue(fftSizesSelect))
+  updateCharts()
+}
 
 const onFrequencyChange = () => {
-  currentFrequencies = UH.getCheckedCheckboxes(frequencyCheckboxes)
-  drawCharts(currentFrequencies, currentGain)
+  currentFrequencies = UH.getCheckedCheckboxes(frequencyCheckboxes).map(Number)
+  updateCharts()
 }
 
 const onGainChange = () => {
-  currentGain = UH.getCheckedRadioButton(gainRadioButtons)
-  drawCharts(currentFrequencies, currentGain)
+  currentGain = Number(UH.getCheckedRadioButton(gainRadioButtons))
+  updateCharts()
 }
 
+UH.setSelectedValue(sampleRateSelect, currentSampleRate)
+UH.setSelectedValue(fftSizesSelect, currentFftSize)
 UH.setCheckedCheckboxes(frequencyCheckboxes, currentFrequencies)
 UH.setCheckedRadioButton(gainRadioButtons, currentGain)
 
-UH.buttonsOnChange(frequencyCheckboxes, onFrequencyChange)
-UH.buttonsOnChange(gainRadioButtons, onGainChange)
+UH.selectOnChange(sampleRateSelect, onSampleRateChange)
+UH.selectOnChange(fftSizesSelect, onFftSizeChange)
+UH.checkboxesOnChange(frequencyCheckboxes, onFrequencyChange)
+UH.radioButtonsOnChange(gainRadioButtons, onGainChange)
 
 const makeOscillatorNode = (audioContext, gainNode) => frequency => {
-  const oscillatorNode = new OscillatorNode(audioContext, { frequency })
+  const oscillatorNode = audioContext.createOscillator()
+  oscillatorNode.frequency.value = frequency
   oscillatorNode.connect(gainNode)
   return oscillatorNode
 }
@@ -52,43 +89,62 @@ const makeOscillatorNode = (audioContext, gainNode) => frequency => {
 const startOscillatorNode = when => oscillatorNode => oscillatorNode.start(when)
 const stopOscillatorNode = when => oscillatorNode => oscillatorNode.stop(when)
 
-const drawCharts = async (frequencies, gain) => {
-  const SAMPLE_RATE = 8000
-  const FFT_SIZE = 1024
-  const options = {
-    length: SAMPLE_RATE,
-    sampleRate: SAMPLE_RATE
+const updateCharts = () =>
+  drawCharts(
+    currentSampleRate,
+    currentFftSize,
+    currentFrequencies,
+    currentGain)
+
+const drawCharts = async (sampleRate, fftSize, frequencies, gain) => {
+  try {
+    UH.hideErrorPanel()
+    const SECONDS = 1
+    const numberOfChannels = 1
+    const length = sampleRate * SECONDS
+    const audioContext = new OfflineAudioContext(numberOfChannels, length, sampleRate)
+    const gainNode = audioContext.createGain()
+    gainNode.gain.value = gain
+    const oscillatorNodes = frequencies.map(makeOscillatorNode(audioContext, gainNode))
+    const analyserNode = audioContext.createAnalyser()
+    analyserNode.fftSize = fftSize
+    gainNode.connect(analyserNode)
+    analyserNode.connect(audioContext.destination)
+    oscillatorNodes.forEach(startOscillatorNode(audioContext.currentTime))
+    oscillatorNodes.forEach(stopOscillatorNode(audioContext.currentTime + SECONDS))
+    await audioContext.startRendering()
+    const timeDomainData = new Uint8Array(analyserNode.frequencyBinCount)
+    const frequencyData = new Uint8Array(analyserNode.frequencyBinCount)
+    analyserNode.getByteTimeDomainData(timeDomainData)
+    analyserNode.getByteFrequencyData(frequencyData)
+    UC.drawTimeDomainChart('timeDomainChart', timeDomainData)
+    UC.drawFFTChart('fftChart', frequencyData, sampleRate)
+    const binSize = sampleRate / fftSize
+    showBins(binSize, frequencies.length, frequencyData)
+  } catch (error) {
+    UH.showErrorPanel(error.message)
   }
-  const audioContext = new OfflineAudioContext(options)
-  const gainNode = new GainNode(audioContext, { gain })
-  const oscillatorNodes = frequencies.map(makeOscillatorNode(audioContext, gainNode))
-  const analyserNode = new AnalyserNode(audioContext, { fftSize: FFT_SIZE })
-  gainNode.connect(audioContext.destination)
-  gainNode.connect(analyserNode)
-  oscillatorNodes.forEach(startOscillatorNode(0))
-  oscillatorNodes.forEach(stopOscillatorNode(1))
-  await audioContext.startRendering()
-  const timeDomainData = new Uint8Array(analyserNode.frequencyBinCount)
-  const frequencyData = new Uint8Array(analyserNode.frequencyBinCount)
-  analyserNode.getByteTimeDomainData(timeDomainData)
-  analyserNode.getByteFrequencyData(frequencyData)
-  UC.drawTimeDomainChart('timeDomainChart', timeDomainData)
-  UC.drawFFTChart('fftChart', frequencyData, SAMPLE_RATE)
-  const binSize = SAMPLE_RATE / FFT_SIZE
-  showBins(binSize, frequencies.length, frequencyData)
 }
 
 const findTopBins = frequencyData => {
   const binValues = Array.from(frequencyData)
-  const zipped = binValues.map((binValue, index) => ({ binValue, bin: index }))
-  return zipped.sort((a, b) => b.binValue - a.binValue)
+  const zipped = binValues.map((value, bin) => ({ value, bin }))
+  return zipped.sort((a, b) => b.value - a.value)
 }
 
 const showBins = (binSize, numFrequencies, frequencyData) => {
   const topBins = findTopBins(frequencyData)
   const topFewBins = R.take(numFrequencies, topBins)
-  const lines = topFewBins.map(({ bin }) =>
-    `bin: ${bin}; frequency range: ${bin * binSize} - ${(bin + 1) * binSize} Hz`)
+  const frequencyRange = bin => {
+    const from = (bin * binSize).toFixed(2).padStart(7)
+    const to = ((bin + 1) * binSize).toFixed(2).padStart(7)
+    return `${from} Hz to ${to} Hz`
+  }
+  const lines = topFewBins.map(({ value, bin }) => {
+    const binStr = bin.toString().padStart(3)
+    const valueStr = value.toString().padStart(3)
+    return `bin: ${binStr}; value: ${valueStr}; frequency range: ${frequencyRange(bin)}`
+  })
   const binsPre = document.getElementById('binsRow').querySelector('pre')
   binsPre.innerHTML = lines.join('\n')
 }
