7dd27daba3670954b14d79eeae5906a12c9b4e09
Administrator <jonathan.taylor.programmer@gmail.com>
2020-02-09 17:55:53 +0000

    Last idea not working on Chrome/iPadOS

--- js/client/experiments/recording-2.js
+++ js/client/experiments/recording-2.js
@@ -59,26 +59,26 @@ const floatToByte = v => Math.round((v + 1) / 2 * 255)
 
 // https://stackoverflow.com/questions/53150556/accessing-microphone-with-the-help-of-getusermedia-on-ios-safari
 
-if (navigator.mediaDevices === undefined) {
-  navigator.mediaDevices = {}
-}
+// if (navigator.mediaDevices === undefined) {
+//   navigator.mediaDevices = {}
+// }
 
-if (navigator.mediaDevices.getUserMedia === undefined) {
-  navigator.mediaDevices.getUserMedia = constraints => {
-    const getUserMedia = (
-      navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia
-    )
+// if (navigator.mediaDevices.getUserMedia === undefined) {
+//   navigator.mediaDevices.getUserMedia = constraints => {
+//     const getUserMedia = (
+//       navigator.getUserMedia ||
+//       navigator.webkitGetUserMedia ||
+//       navigator.mozGetUserMedia
+//     )
 
-    if (!getUserMedia) {
-      return Promise.reject(new Error('getUserMedia is not implemented in this browser'))
-    }
+//     if (!getUserMedia) {
+//       return Promise.reject(new Error('getUserMedia is not implemented in this browser'))
+//     }
 
-    return new Promise((resolve, reject) =>
-      getUserMedia.call(navigator, constraints, resolve, reject))
-  }
-}
+//     return new Promise((resolve, reject) =>
+//       getUserMedia.call(navigator, constraints, resolve, reject))
+//   }
+// }
 
 /* -------------------------------------------------------------------------- */
 
